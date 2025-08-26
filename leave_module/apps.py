c5ff6e129e595d48a59b1061ec89c790baae5367
manish <mahasethmanish63@gmail.com>
2025-08-26 10:23:44 +0545

    git commit

--- leave_module/apps.py
+++ leave_module/apps.py
@@ -1,29 +1,28 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
-def create_default_superuser():
-     from decouple import config
-     from django.contrib.auth import get_user_model
-     from django.db.utils import OperationalError
-     
-     User = get_user_model()
-     
-     try:
-         username = config("DJANGO_SUPERUSER_USERNAME", default="admin")
-         email = config("DJANGO_SUPERUSER_EMAIL", default="admin@example.com")
-         password = config("DJANGO_SUPERUSER_PASSWORD", default="admin123")
-     
-         if not User.objects.filter(username=username).exists():
-             User.objects.create_superuser(username=username, email=email, password=password)
-             print(f"✅ Superuser {username} created.")
-     except OperationalError:
-         # Happens during migrations before DB is ready → ignore
-         pass
+from django.contrib.auth import get_user_model
 
 class LeaveModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'leave_module'
 
-
     def ready(self):
+        User = get_user_model()
+
+        def create_default_superuser(sender, **kwargs):
+            from decouple import config
+            from django.db.utils import OperationalError
+
+            try:
+                username = config("DJANGO_SUPERUSER_USERNAME", default="admin")
+                email = config("DJANGO_SUPERUSER_EMAIL", default="admin@example.com")
+                password = config("DJANGO_SUPERUSER_PASSWORD", default="admin123")
+
+                if not User.objects.filter(username=username).exists():
+                    User.objects.create_superuser(username=username, email=email, password=password)
+                    print(f"✅ Superuser {username} created.")
+            except OperationalError:
+                # Happens during migrations before DB is ready → ignore
+                pass
+
         post_migrate.connect(create_default_superuser, sender=self)
-          

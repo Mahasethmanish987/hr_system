40345c76efc80f15592bbc96d7db0a1253920920
manish <mahasethmanish63@gmail.com>
2025-08-26 11:45:43 +0545

    change the commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         "NAME": os.environ.get("DATABASE_NAME"),
         "USER": os.environ.get("DATABASE_USER"),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
+        "HOST": os.environ.get("DATABASE_HOST"),
         "PORT": int(os.environ.get("DATABASE_PORT", 5432)),
     }
 }
